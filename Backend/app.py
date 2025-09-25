6aa2ba3ae33928db92116af0494f5f060b0b7f99
TheRealShreyash <koshta.shreyash02@gmail.com>
2025-09-25 10:30:11 +0530

    adding cors to the api

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from datetime import datetime
 import bcrypt
 
 app = Flask(__name__)
+CORS(app)
 
 # In-memory storage for complaints
 complaintStore = {}
